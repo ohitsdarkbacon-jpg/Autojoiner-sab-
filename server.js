7f6c144224991effdaae16bca6fa9136afc5d96e
ohitsdarkbacon-jpg <ohitsdarkbacon@gmail.com>
2026-04-02 23:35:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ setInterval(() => {
     }
 }, 30000);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
     console.log(`HTTP Server running on port ${PORT} (/gettoken)`);
 });
